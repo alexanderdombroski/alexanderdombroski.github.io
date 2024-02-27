c7aad5eca44e6f095671ebc11f75270765c3638a
alexanderdombroski <dom21013@byui.edu>
2024-02-26 18:35:04 -0700

    faction, cost, and defense are now populated into table when a card is selected.

--- cardgames/sr-calculator/scripts/update-row.js
+++ cardgames/sr-calculator/scripts/update-row.js
@@ -16,31 +16,54 @@ function getColor(id) {
             return "#990000" // Red
         case "11":
             return "black"
+        case "12": // All factions
+            return "#fc5e03" // red-orange
         default:
             console.log("error in color selection id=", id)
     }
-};
+}
 
-function transferDataFromDatalist(target, targetValue) {
+function getFaction(id) {
+    switch (id) {
+        case "1":
+            return "trade" // Blue
+        case "2":
+            return "blob" // Green
+        case "3":
+            return "star" // Yellow
+        case "4":
+            return "mech" // Red
+        case "11":
+            return "none"
+        case "12": // All factions
+            return "multi" // red-orange
+        default:
+            console.log("error in color selection id=", id)
+            return "IDK"
+    }
+}
+
+function transferDataFromDatalist(target, targetValue, cells) {
     // Check if the ship is in either datalist, if so, transfer the data attributes of the selected option
     const shipDataList = document.getElementById("filtered-card-list");
     const removedDataList = document.getElementById("removed-card-list");
-    
+
     var optionNotFound = true;
     function checkDatalist(datalist) {
         const options = datalist.querySelectorAll('option')
         options.forEach(option => {
             if (option.value === targetValue) {
-                target.setAttribute("deck", option.getAttribute("deck"));
                 target.setAttribute("faction", option.getAttribute("faction"));
-                // target.setAttribute("cost", option.getAttribute("cost"));
-                // target.setAttribute("defense", option.getAttribute("defense"));
+                cells[0].innerHTML = getFaction(option.getAttribute("faction"));
+                cells[1].innerHTML = option.getAttribute("cost");
+                cells[9].innerHTML = option.getAttribute("defense");
                 optionNotFound = false;
             }
         });
     }
     checkDatalist(shipDataList)
     optionNotFound && checkDatalist(removedDataList);
+    // Remove the card if it doesn't exist in the datalists
     if (optionNotFound) {
         target.value = "";
     }
@@ -50,12 +73,12 @@ function handleCardChange(event) {
     // Access the selected value using event.target.value
     const selectedValue = event.target.value;
     const rowNumber = event.target.className.split(' ')[1];
+    const cells = document.querySelectorAll(`.${rowNumber} td`);
     
     // Verify user input and delete if 
-    transferDataFromDatalist(event.target, selectedValue);
+    transferDataFromDatalist(event.target, selectedValue, cells);
 
     // Change color
-    const cells = document.querySelectorAll(`.${rowNumber} td`);
     const factionId = event.target.getAttribute("faction");
     const color = getColor(factionId);
     event.target.style.color = color;
