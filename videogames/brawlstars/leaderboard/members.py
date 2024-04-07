94cf51233c2bd5b2cd05b79fd2e3f13f1377f059
alexanderdombroski <dom21013@byui.edu>
2024-04-07 09:33:22 -0600

    Moved python files to a local storage

--- videogames/brawlstars/leaderboard/members.py
+++
@@ -1,9 +0,0 @@
-from member_data_parser import GetJsonFile
-
-def main():
-    members = GetJsonFile("data/members.json")
-    for key, name in members.items():
-        print(f"{key}: {name}")
-
-if __name__ == "__main__":
-    main()
