651fa73f18ecdcf88cf48e2e0723ed8f77454719
Alex Dombroski <alexdombroski1@gmail.com>
2026-01-10 15:29:39 -0700

    feat: add pokemon sound player and image

--- videogames/pokemon/scripts/main.mjs
+++ videogames/pokemon/scripts/main.mjs
@@ -1,4 +1,9 @@
 import { loadNameSuggestions } from "./pokemonList.mjs";
+import { updatePokemonData } from "./pokemonData.mjs";
 
-loadNameSuggestions()
+loadNameSuggestions();
 
+const selector = document.getElementById("pokemon-search");
+selector.addEventListener("change", (event) =>
+  updatePokemonData(event.currentTarget.value)
+);
