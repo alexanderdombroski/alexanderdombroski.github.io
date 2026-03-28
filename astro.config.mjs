918de9c1ab59ae3f382323e2f65434b88b541db4
Alex Dombroski <alexdombroski1@gmail.com>
2026-03-27 22:19:00 -0600

    feat: add git guide page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,37 @@
 // @ts-check
 
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig, fontProviders } from "astro/config";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from 'astro/config';
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alexdombroski.com",
+  site: 'https://alexdombroski.com',
   integrations: [mdx(), sitemap(), svelte()],
+  devToolbar: {
+    enabled: false,
+  },
   fonts: [
     {
       provider: fontProviders.local(),
-      name: "Atkinson",
-      cssVariable: "--font",
-      fallbacks: ["sans-serif"],
+      name: 'Atkinson',
+      cssVariable: '--font',
+      fallbacks: ['sans-serif'],
       options: {
         variants: [
           {
-            src: ["./src/assets/fonts/atkinson-regular.woff"],
+            src: ['./src/assets/fonts/atkinson-regular.woff'],
             weight: 400,
-            style: "normal",
-            display: "swap",
+            style: 'normal',
+            display: 'swap',
           },
           {
-            src: ["./src/assets/fonts/atkinson-bold.woff"],
+            src: ['./src/assets/fonts/atkinson-bold.woff'],
             weight: 700,
-            style: "normal",
-            display: "swap",
+            style: 'normal',
+            display: 'swap',
           },
         ],
       },
